046cbd71166ebe4bb58b68653afd802a2e6911c3
Jose Adrian <kerpador@gmail.com>
2024-07-24 01:57:31 -0600

    adding the carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Header from './components/Header'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import axios from "axios"
+import Carrousel from "./components/carrousel";
+const slides=[
+  "/src/assets/Fachaletas-6.jpg",
+  "/src/assets/full_Teja_Tipo_S_Terracota.jpg",
+  "/src/assets/image_processing20201110-4-18n6g9t.jpg",
+]
 function App() {
  const [count, setCount] = useState(0);
  const  [array,setArray] =useState([]);
@@ -22,10 +28,20 @@ function App() {
   duration: 1000, // Global animation duration
   });
   }, []);
-  return (
-    <div className='container'>
-      <Header />
+  return (<>
+    <div className='container h-[13vh] relative z-10'>
+      <Header>
+      </Header>
     </div>
+    <main className="container grid relative justify-center items-center content-center" >
+        <div className="flex items-center justify-center w-[60vw] ">
+          <Carrousel autoslide={true} autoslideInterval={2000}>
+            {slides.map((s)=>(
+              <img src={s} />
+            ))}
+          </Carrousel>
+        </div>
+      </main></>
   )
 }
 
